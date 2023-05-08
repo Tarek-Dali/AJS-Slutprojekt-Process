e4675fc3a40d270fd1f7992759012b0d320056f2
Tarek <ironhead2050@gmail.com>
2023-05-08 23:20:02 +0200

    Prices are better explained now in ShoppingCart

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -5,12 +5,13 @@ export default function ShoppingCart({ functions, data, amount }) {
 
     // Adds elements with every added product's info and calculates total price
     const productsAddedTextEl = [];
-    let price = 0;
+    let totalPrice = 0;
 
     for (let i = 0; i < data.length; i++) {
         if (amount[i] != 0) {
-            productsAddedTextEl.push(<h2 key={i}>{amount[i]} {data[i].name}</h2>);
-            price += amount[i] * data[i].price;
+            const price = amount[i] * data[i].price;
+            productsAddedTextEl.push(<h2 key={i}>{amount[i]} {data[i].name} for {price} kr</h2>);
+            totalPrice += amount[i] * data[i].price;
         }
     }
 
@@ -20,7 +21,7 @@ export default function ShoppingCart({ functions, data, amount }) {
             <div className="shoppingCartText">
                 <h2 id="shoppingCartTitle">Shopping cart </h2>
                 {productsAddedTextEl}
-                <h2>to the cart and the total price is {price} kr</h2>
+                <h2>to the cart and the total price is {totalPrice} kr</h2>
 
                 <CompletePurchase functions={functions} />
                 <EmptyCartButton functions={functions} />
